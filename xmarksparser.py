8bc78a8cc43395b76950759f05fee6c2d8edfcd6
Anton (Atilla) Tsigularov <antont@opera.com>
2010-11-18 11:10:52 +0100

    Adding documentation, and a simple doctest. Some small formatting fixes.

--- xmarksparser.py
+++ xmarksparser.py
@@ -1,16 +1,33 @@
 from HTMLParser import HTMLParser
 
+
 class XMarksParser(HTMLParser):
+    """
+    Processes HTML data in Xmarks' format.
+    Returns a representation of this data as a python dictionary
+
+    >>> html = u"<dl><dt><a href='www.opera.com'>Opera</a></dt><dl/>"
+    >>> parser = XMarksParser()
+    >>> parser.feed(html)
+    >>> parser.bms
+    [{'item_type': 'bookmark', 'properties': {'uri': u'www.opera.com', 'title': u'Opera'}}]
+    """
 
     def __init__(self):
         HTMLParser.__init__(self)
+        # The actual result of the parsing
         self.bms = []
+        # Run stack for the parsing
         self._item_stack = [self.bms, ]
+        # State of the state-machine
         self._state = "start"
 
     def append_item(self, item):
-        # In the root, we simply append the item,
-        # otherwise we have to add to to children
+        """
+        Depending on the state of the machine, appends the item either
+        to the topmost list of results or in a list under the
+        'children' key
+        """
         if self._state == "top":
             self._item_stack[-1].append(item)
         elif self._state == "folder":
@@ -19,12 +36,17 @@ class XMarksParser(HTMLParser):
                 parent["children"] = []
             parent["children"].append(item)
 
-
     def handle_starttag(self, tag, attrs):
+        """
+        Called on every start tag found
+        """
+
+        #Handles the first <dl> that encapsulates all items
         if tag == "dl":
             if self._state == "start":
                 self._state = "top"
 
+        # <h3> defines a new folder. The data in it is the title
         elif tag == "h3":
             folder = {
                 "item_type": "bookmark_folder",
@@ -35,11 +57,13 @@ class XMarksParser(HTMLParser):
             self._item_stack.append(folder)
             self._state = "foldertitle"
 
+        # <a> defines a new bookmark. The href is the URI.
         elif tag == "a":
             bookmark = {
                 "item_type": "bookmark",
                 "properties": {},
                 }
+            # Find the href attrib and assign it to URI
             for name, value in attrs:
                 if name == "href":
                     bookmark["properties"]["uri"] = value
@@ -48,9 +72,14 @@ class XMarksParser(HTMLParser):
             self._item_stack.append(bookmark)
 
     def handle_endtag(self, tag):
+        """
+        Called on every end tag found
+        """
+        # End of bookmark or folder definition.
         if tag in ("dl", "a"):
             item = self._item_stack.pop()
 
+            # In case of missing titles, assign a default one
             if isinstance(item, dict) and "title" not in item["properties"]:
                 item["properties"]["title"] = "No Title"
 
@@ -61,5 +90,8 @@ class XMarksParser(HTMLParser):
                 self._state = "folder"
 
     def handle_data(self, data):
+        """
+        Called when data is found inside the tags.
+        """
         if self._state in ("foldertitle", "bookmark"):
             self._item_stack[-1]["properties"]["title"] = data
